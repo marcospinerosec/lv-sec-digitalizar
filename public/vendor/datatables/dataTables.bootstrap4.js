ccd5d6f0a976f077e693da9756e7a7894ba2091d
marcos.piñero <marcosp@presi.unlp.edu.ar>
2021-11-04 17:18:55 -0300

    formato de dataTable

--- public/vendor/datatables/dataTables.bootstrap4.js
+++ public/vendor/datatables/dataTables.bootstrap4.js
@@ -46,9 +46,9 @@ var DataTable = $.fn.dataTable;
 /* Set the defaults for DataTables initialisation */
 $.extend( true, DataTable.defaults, {
 	dom:
-		"<'row'<'col-sm-12 col-md-6'l><'col-sm-12 col-md-6'f>>" +
+		"<'row'<'col-sm-12 col-md-6'l><'col-sm-12 col-md-4'f>>" +
 		"<'row'<'col-sm-12'tr>>" +
-		"<'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7'p>>",
+		"<'row'<'col-sm-12 col-md-6'i><'col-sm-12 col-md-7'p>>",
 	renderer: 'bootstrap'
 } );
 
@@ -157,7 +157,7 @@ DataTable.ext.renderer.pageButton.bootstrap = function ( settings, host, idx, bu
 	};
 
 	// IE9 throws an 'unknown error' if document.activeElement is used
-	// inside an iframe or frame. 
+	// inside an iframe or frame.
 	var activeEl;
 
 	try {
